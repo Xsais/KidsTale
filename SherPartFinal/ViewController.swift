ad5f9990a1adc4109b19acd51021fe26a44fd5d5
Sher <38057955+khasherr@users.noreply.github.com>
2020-04-11 17:10:46 -0400

    Added launch screen storyboard and main storyboard
    added launchstoryboard and mainstoryboard

---
+++ SherPartFinal/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SherPartFinal
+//
+//  Created by Xcode User on 2020-04-11.
+//  Copyright © 2020 Xcode User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var spinner : UIActivityIndicatorView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //spinner.startAnimating();
+    }
+
+
+}
+
