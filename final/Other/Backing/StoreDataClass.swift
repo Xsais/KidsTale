0e80ced3b2c37a74b12cc8c19fabb812618c1324
Nathaniel Primo <nathaniel.primo@gmail.com>
2020-04-14 09:11:33 -0400

    Re-enabled the menu on the home screen

--- final/Other/Backing/StoreDataClass.swift
+++
@@ -1,9 +0,0 @@
-//
-//  StoreDataClass.swift
-//  IOSFinalProjectStoreDetails
-//
-//  Created by Xcode User on 2020-04-11.
-//  Copyright © 2020 Xcode User. All rights reserved.
-//
-
-import UIKit
--- final/View Controllers/Home Screen/HomeController.swift
+++ final/View Controllers/Home Screen/HomeController.swift
@@ -146,15 +146,13 @@ public class HomeController: SmartUIViewController, UITableViewDelegate, UITable
             // TODO: present() additional information for the specific book
         } else if (viewing == Store.self) {
 
-            // TODO: present() additional information for the specific store
+            //redirect the page to Store Details  and store the neccessary data into AppDelegate( Jie Ming Wu)
+            //store the select data into appdalegate's object, this object will be use to pull out the store details from db
+            mainDelegate.storeSelect = selectedItem?.name
+            self.performSegue(withIdentifier: "storeDetailViewController", sender: self)
+
+            //end (Jie Ming Wu)
         }
-    
-       //redirect the page to Store Details  and store the neccessary data into AppDelegate( Jie Ming Wu)
-       //store the select data into appdalegate's object, this object will be use to pull out the store details from db
-       mainDelegate.storeSelect = selectedItem?.name
-       self.performSegue(withIdentifier: "storeDetailViewController", sender: self)
-    
-        //end (Jie Ming Wu)
     }
 
 }
